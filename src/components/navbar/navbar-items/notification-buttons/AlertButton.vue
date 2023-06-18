df2da287bb12c3e84c887a26e5dcb5f0c7c58c1d
juanjaroa <juanjaroa@users.noreply.github.com>
2023-06-18 00:37:07 +0000

    deploy: d1fb94a6d33ca286dc7d4eb9ad439aff4bc0d8a0

--- src/components/navbar/navbar-items/notification-buttons/AlertButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-<template>
-    <button>
-        <img class="icon" src="../../assets/ic_bell.png" alt="">
-        <div class="badge"></div>
-    </button>
-</template>
-
-<style scoped></style>
